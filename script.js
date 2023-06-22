7a3ebb3980a5193cd5ecb33558d0dbec9cae339f
Yerson Trujillo <yersontrujilloyacha@gmail.com>
2023-06-22 16:52:48 -0500

    web detects browser language to display content

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-// Retrieve the user's language preference from local storage
-const preferredLanguage = localStorage.getItem("language");
-
 // Get the language sections
 const englishSection = document.getElementById("english");
 const spanishSection = document.getElementById("spanish");
 
+// Get user's browser preferred language
+const browserLanguage = navigator.languages[0].startsWith("en")
+  ? "english"
+  : navigator.languages[0].startsWith("es")
+  ? "spanish"
+  : "other";
+
+console.log("browser language is " + browserLanguage);
+
+// Retrieve the user's language preference from local storage
+const localLanguage = localStorage.getItem("language");
+
 // Get the select element
 const selectElement = document.querySelector(".select-wrapper");
+
+// Set preferred language, browser language by default
+let preferredLanguage =
+  localStorage.getItem("language") != null
+    ? localStorage.getItem("language")
+    : localStorage.getItem("language") == null
+    ? browserLanguage
+    : "problems";
+
+console.log("language stored in memory is " + localStorage.getItem("language"));
 
 // Set the selected option based on the preferred language
 selectElement.value = preferredLanguage;
 
-// Show the appropriate language section based on the user's preference
-if (preferredLanguage === "spanish") {
-  spanishSection.style.display = "block";
-} else {
-  englishSection.style.display = "block";
-}
+console.log("preferred lang is " + preferredLanguage);
 
 // Function to change the language
 function changeLanguage(language) {
   // Update the selected option
   selectElement.value = language;
 
+  // Show the appropriate language section based on the user's preference
   if (language === "spanish") {
     spanishSection.style.display = "block";
     englishSection.style.display = "none";
-  } else {
+  } else if (language === "english") {
+    spanishSection.style.display = "none";
     englishSection.style.display = "block";
+  } else {
+    englishSection.style.display = "none";
     spanishSection.style.display = "none";
   }
 
